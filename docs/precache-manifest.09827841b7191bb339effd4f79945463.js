55550eef1a10707abf955bfe02f06b294f2f77ae
Peter Kottas <petokottas@gmail.com>
2018-12-19 19:22:22 +0100

    Added docs and fixed readme

---
+++ docs/precache-manifest.09827841b7191bb339effd4f79945463.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "ee1a3a4d20c573a6c169",
+    "url": "/static/css/main.d24c6a80.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "7e2703b1ad3ee0fb22b4",
+    "url": "/static/js/1.7e2703b1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ee1a3a4d20c573a6c169",
+    "url": "/static/js/main.ee1a3a4d.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "7e2703b1ad3ee0fb22b4",
+    "url": "/static/css/1.40ca0e76.chunk.css"
+  },
+  {
+    "revision": "9cffac145f7f6aa86f7eb4ad5aaf3e10",
+    "url": "/index.html"
+  }
+];
