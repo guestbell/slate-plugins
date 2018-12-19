44e09e0c034822f10fcffadc05498d708602ed2e
Peter Kottas <petokottas@gmail.com>
2018-12-19 19:27:16 +0100

    Fixing docs

---
+++ docs/precache-manifest.5fb65105fc85e371cff591783ba0806d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/slate-plugins/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "4ae7bc3fbabbc4660431",
+    "url": "/slate-plugins/static/css/main.d24c6a80.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/slate-plugins/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "62dab995c52056e3c74c",
+    "url": "/slate-plugins/static/js/1.62dab995.chunk.js"
+  },
+  {
+    "revision": "121fb6e3145020a961dd",
+    "url": "/slate-plugins/static/js/runtime~main.121fb6e3.js"
+  },
+  {
+    "revision": "4ae7bc3fbabbc4660431",
+    "url": "/slate-plugins/static/js/main.4ae7bc3f.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/slate-plugins/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/slate-plugins/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/slate-plugins/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "62dab995c52056e3c74c",
+    "url": "/slate-plugins/static/css/1.4b8f6553.chunk.css"
+  },
+  {
+    "revision": "2c14ea5904c8451bccb304429566431c",
+    "url": "/slate-plugins/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.09827841b7191bb339effd4f79945463.js"
+  "/slate-plugins/precache-manifest.5fb65105fc85e371cff591783ba0806d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/slate-plugins/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
